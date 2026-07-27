de06b4a835fc4601ad908f562b7d73c74c679c6c
Rishabh-020 <rishabh5824sjps@gmail.com>
2026-07-27 14:29:18 +0530

    (security,dto,controller) : completed the whole JWT work flow for the login and sign process

---
+++ backend/src/main/java/com/enera/backend/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.enera.backend.controller;
+
+import com.enera.backend.dto.LoginRequest;
+import com.enera.backend.dto.LoginResponse;
+import com.enera.backend.security.JwtService;
+import io.jsonwebtoken.Jwt;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthenticationManager  authenticationManager;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public AuthController(AuthenticationManager authenticationManager,
+                   JwtService jwtService, UserDetailsService userDetailsService){
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword()));
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+
+        String token = jwtService.generateToken(userDetails);
+
+        return new LoginResponse(token);
+    }
+
+}
